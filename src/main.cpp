a25ac6e20e93863d6ac321b2e618fe4e6642e1a6
Bhushan Shah <bhush94@gmail.com>
2017-12-15 20:19:35 +0530

    Fix aboutdata so plasmashell is not blocked on it

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv)
     KLocalizedString::setApplicationDomain("plasma-settings");
 
     // About data
-    KAboutData aboutData("plasmamobilesettings", i18n("Settings"), version, i18n("Touch-friendly settings application."), KAboutLicense::GPL, i18n("Copyright 2011-2015, Sebastian Kügler"));
+    KAboutData aboutData("mobile.plasmasettings", i18n("Settings"), version, i18n("Touch-friendly settings application."), KAboutLicense::GPL, i18n("Copyright 2011-2015, Sebastian Kügler"));
     aboutData.addAuthor(i18n("Sebastian Kügler"), i18n("Maintainer"), "sebas@kde.org");
     aboutData.addAuthor(i18n("Marco Martin"), i18n("Maintainer"), "mart@kde.org");
     aboutData.setDesktopFileName("org.kde.mobile.plasmasettings");
