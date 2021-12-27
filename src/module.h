2bd8d0b9e98c32a8a8474f5f0c2e7c4950299cba
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-12-27 09:06:24 +0100

    Module: Add name property for source code compat of plasma-bigscreen

--- src/module.h
+++ src/module.h
@@ -15,15 +15,22 @@ class Module : public QObject
     Q_OBJECT
     Q_PROPERTY(KQuickAddons::ConfigModule *kcm READ kcm NOTIFY kcmChanged)
     Q_PROPERTY(QString path READ path WRITE setPath NOTIFY pathChanged)
+    Q_PROPERTY(QString name READ name WRITE setPath NOTIFY nameChanged)
 
 public:
     KQuickAddons::ConfigModule *kcm() const;
     QString path() const;
     void setPath(const QString &name);
 
+    QString name()
+    {
+        return QFileInfo(m_path).baseName();
+    }
+
 Q_SIGNALS:
     void kcmChanged();
     void pathChanged();
+    void nameChanged();
 
 private:
     KQuickAddons::ConfigModule *m_kcm = nullptr;
