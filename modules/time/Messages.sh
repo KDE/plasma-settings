fac86d341ffca0de3cfc2407c6eb0cb2c0b1f8fd
Devin Lin <espidev@gmail.com>
2023-03-13 19:13:06 -0700

    modules: Remove all KCMs KCMs are moved to the Plasma Mobile repo: https://invent.kde.org/plasma/plasma-mobile/-/merge_requests/319

--- modules/time/Messages.sh
+++
@@ -1,1 +0,0 @@
-$XGETTEXT $(find . -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/kcm_mobile_time.pot
--- modules/time/config.h.cmake
+++
@@ -1,9 +0,0 @@
-/*
-    SPDX-FileCopyrightText: 2011 Marco Martin <mart@kde.org>
-
-    SPDX-License-Identifier: LGPL-2.0-or-later
-*/
-
-/* KDE's configuration directory */
-#define KDE_CONFDIR "${CONFIG_INSTALL_DIR}"
-
